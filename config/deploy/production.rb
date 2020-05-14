031e4fed396e438ad48bcd77a83662a6f702bfa6
daisaku kato <50142017+katkic@users.noreply.github.com>
2020-05-14 20:24:15 +0900

    deploy_test_second

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,4 @@
-server '18.178.15.21', user: 'app', roles: %w{app db web}
+server '54.95.236.248', user: 'app', roles: %w{app db web}
 set :ssh_options, keys: '/Users/mac/.ssh/id_rsa'
+# server '18.178.15.21', user: 'app', roles: %w{app db web}
+# set :ssh_options, keys: '/Users/mac/.ssh/id_rsa'
